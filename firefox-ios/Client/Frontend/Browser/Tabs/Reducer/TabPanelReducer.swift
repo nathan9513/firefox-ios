de17d8b2f149b44b6effed05e63e136979b8c453
nathan9513 <nathangiovannini3@gmail.com>
2025-01-29 19:31:07 +0100

    fix

--- firefox-ios/Client/Frontend/Browser/Tabs/Reducer/TabPanelReducer.swift
+++ firefox-ios/Client/Frontend/Browser/Tabs/Reducer/TabPanelReducer.swift
@@ -19,33 +19,7 @@ static func reduceTabPanelViewAction(action: TabPanelViewAction, state: TabTrayS
     }
 }
 
-// Assicurati che la funzione defaultState sia definita
 func defaultState(from state: TabTrayState) -> TabTrayState {
     // Implementa la logica per restituire lo stato predefinito
-    return state
-}static func reduceTabPanelViewAction(action: TabPanelViewAction, state: TabTrayState) -> TabTrayState {
-    switch action.actionType {
-    case .confirmCloseAllTabs:
-        return TabTrayState(windowUUID: state.windowUUID,
-                            isPrivateMode: state.isPrivateMode,
-                            selectedPanel: state.selectedPanel,
-                            normalTabsCount: state.normalTabsCount,
-                            hasSyncableAccount: state.hasSyncableAccount,
-                            showCloseConfirmation: true)
-    case .cancelCloseAllTabs:
-        return TabTrayState(windowUUID: state.windowUUID,
-                            isPrivateMode: state.isPrivateMode,
-                            selectedPanel: state.selectedPanel,
-                            normalTabsCount: state.normalTabsCount,
-                            hasSyncableAccount: state.hasSyncableAccount,
-                            showCloseConfirmation: false)
-    default:
-        return defaultState(from: state)
-    }
-}
-
-
-func defaultState(from state: TabTrayState) -> TabTrayState {
-    
     return state
 }
